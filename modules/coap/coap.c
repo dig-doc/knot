0bfd0c55847a5691cf44445e51871a7dfb8e9ae7
Fabian Krusch <Fabian.Krusch@mailbox.tu-dresden.de>
2024-12-27 19:56:05 +0000

    .

--- modules/coap/coap.c
+++ modules/coap/coap.c
@@ -4,97 +4,65 @@
 #include <string.h>
 #include <coap3/coap.h>
 #include <ldns/ldns.h>
-#include "lib/resolve.h" 
 #include "lib/module.h"
-#include "daemon/worker.h"
-#include "lib/rplan.h"
 
 
-// TODO remove later with Markus function
-extern struct kr_context *the_resolver;
-
-// TODO replace with Markus function, currently only for debugging purposes
-void resolveQuestion(knot_dname_t *qname, ldns_rr_type rr_type, ldns_rr_class rr_class) {
+int resolveQuestion(char *qname, ldns_rr_type rr_type, ldns_rr_class rr_class, coap_session_t *session, coap_pdu_t *response) {
     printf("[DEBUG] Starting resolveExample()\n");
 
-    // create request
-    struct kr_request req = {0};
-    req.pool.ctx = mp_new(4096);
+    ldns_resolver *res;     
+    ldns_rdf *ns;           
+    ldns_buffer *buf;
 
-    if (!req.pool.ctx) {
-        printf("[ERROR] Failed to create memory pool\n");
-        return;
-    }
-    req.pool.alloc = (knot_mm_alloc_t)mp_alloc;
+    // point to knot-resolver
+    ns = ldns_rdf_new_frm_str(LDNS_RDF_TYPE_A, "127.0.0.1");
+    res = ldns_resolver_new();
+    ldns_resolver_push_nameserver(res, ns);
+    ldns_resolver_set_port(res, 53);
 
-    // create packet and link it to request
-    knot_pkt_t *pkt = knot_pkt_new(NULL, KNOT_WIRE_MAX_PKTSIZE, &req.pool);
-    if (!pkt) {
-        printf("[ERROR] Failed to create packet\n");
-        mp_delete(req.pool.ctx);
-        return;
-    }
-    req.qsource.packet = pkt; 
-    printf("[DEBUG] Packet created successfully\n");
-
-    // add question-section to packet 
-    // TODO use rr_type and rr_class
-    int ret = knot_pkt_put_question(pkt, qname, rr_class, rr_type);
-    if (ret != KNOT_EOK) {
-        printf("[ERROR] Failed to put question in packet, error: %d\n", ret);
-        return;
+    // check if resolver is set
+    if(!res) {
+        printf("[ERROR] Failed to create resolver\n");
+        return kr_ok();
     }
 
-    // start knot resolvers resolution process 
-    printf("[DEBUG] Starting resolution process \n");
-    int state = kr_resolve_begin(&req, the_resolver);
-    if (state != KR_STATE_CONSUME) {
-        printf("[ERROR] Failed resolution, state: %d\n", state);
-        mp_delete(req.pool.ctx);
-        return;
-    }
-
-    // state should be PRODUCE after consume - currently not working :( 
-    state = kr_resolve_consume(&req, NULL, pkt);
-    printf("[DEBUG] Consume state: %d\n", state);
+    // dns paket
+    ldns_rdf *domain = ldns_dname_new_frm_str(qname);
+    ldns_pkt *q = ldns_pkt_query_new(domain, rr_type, rr_class, LDNS_RD);
+    buf = ldns_buffer_new(512);
+    ldns_pkt2buffer_wire(buf, q);
     
-    // Generate answer
-    while (state == KR_STATE_PRODUCE) {
-        state = kr_resolve_produce(&req, NULL, req.answer);
-        while (state == KR_STATE_CONSUME){
-            state = kr_resolve_consume(&req, NULL, req.answer);
-        }
+    // dns answer
+    ldns_pkt *answer;
+    ldns_status s = ldns_resolver_send_pkt(&answer, res, q);
+    if (s != LDNS_STATUS_OK) {
+        printf("Error: %s\n", ldns_get_errorstr_by_id(s));
+        return kr_ok();
     }
-    
-    // kr_request_ensure_answer(&req);
-    kr_resolve_finish(&req, state);
 
-    // TODO imrpove output
-    // print result
-    printf("Response code: %d\n", req.answer->wire[3] & 0x0F); // lower 4 bits
-    if (state == KR_STATE_DONE) {
-        printf("\n=== Resolution Result ===\n");
-        printf("Transaction ID: 0x%02x%02x\n", req.answer->wire[0], req.answer->wire[1]);
-        printf("Flags: 0x%02x\n", req.answer->wire[2]);
-        printf("Response code: %d\n", req.answer->wire[3] & 0x0F); // lower 4 bits
-        
-        const knot_pktsection_t *ans = knot_pkt_section(req.answer, KNOT_ANSWER);
-        printf("\nAnswer section %d:\n", ans->count);
-        for (uint16_t i = 0; i < ans->count; i++) {
-            const knot_rrset_t *rr = knot_pkt_rr(ans, i);
-            printf("Name: %s\n",rr->ttl);
-            char buff[512] = {0};
-            size_t buff_len = sizeof(buff);
-            int ret = knot_rrset_txt_dump(rr, buff, &buff_len, &KNOT_DUMP_STYLE_DEFAULT);
-            if (ret == 0) {
-                printf("%s", buff);
-            }
-        }
-    } else {
-        printf("Final state: %d\n", state);
-    }
-    kr_resolve_finish(&req, state);
-    mp_delete(req.pool.ctx);
+    // create new empty buffer
+    ldns_buffer *bufdns = ldns_buffer_new(512); 
+
+    // copy answer-data to buffer
+    ldns_pkt2buffer_wire(bufdns, answer); 
+    uint8_t *data = ldns_buffer_begin(bufdns);
+    size_t len = ldns_buffer_position(bufdns);
+
+    // add data befor headers - to improve speed
+    coap_add_data(response, len, data);
+    coap_pdu_set_code(response, COAP_RESPONSE_CODE_CONTENT);
+
+    // add headers    
+    coap_optlist_t *optlist = NULL;
+    unsigned char buffer_coap[512];
+    len = coap_encode_var_safe(buffer_coap, 512, 553);
+    coap_insert_optlist(&optlist, coap_new_optlist(COAP_OPTION_CONTENT_FORMAT, len, buffer_coap));
+    coap_insert_optlist(&optlist, coap_new_optlist(COAP_OPTION_ACCEPT, len, buffer_coap));
+    coap_add_optlist_pdu(response, &optlist);
+    
+    // print answer
+    ldns_pkt_print(stdout, answer);
+    printf("\n");
     printf("[DEBUG] resolve completed\n");
 }
 
@@ -103,8 +71,8 @@ static void handler_coap_request(coap_resource_t *resource, coap_session_t *sess
     const uint8_t *buffer;
     size_t len, off, total;
 
+    // no data in pdu - do nothing
     if (!coap_get_data_large(receivedPdu, &len, &buffer, &off, &total)) {
-        printf("No response.\n");
         return;
     }
 
@@ -133,17 +101,7 @@ static void handler_coap_request(coap_resource_t *resource, coap_session_t *sess
     
     printf("Domain: \n %s, Type: %d \n Class: %d \n", domain_str, rr_type, rr_class);
 
-    
-    knot_dname_t *qname = knot_dname_from_str_alloc(domain_str);
-    if (!qname) {
-        printf("[ERROR] Failed to allocate qname\n");
-        free(domain_str); 
-        return;
-    }
-    free(domain_str); 
-
-    // resolve requested record and return resolved data to client
-    resolveQuestion(qname, rr_type, rr_class);
+    resolveQuestion(domain_str, rr_type, rr_class, session, response);
 
 }
 
